57effc913dd8154c3726634f8bcca9f2e8d95b42
YuiProg <javierpatrick79@gmail.com>
2026-08-01 13:02:02 +0800

    cors fix

--- taskmaster-backend/task-master-gateway/src/main/java/com/example/gateway_service/gateway/config/SecurityConfig.java
+++ taskmaster-backend/task-master-gateway/src/main/java/com/example/gateway_service/gateway/config/SecurityConfig.java
@@ -33,9 +33,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://*.onrender.com", "http://localhost:8090"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "http://localhost:8090",
+                "https://*.onrender.com"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
