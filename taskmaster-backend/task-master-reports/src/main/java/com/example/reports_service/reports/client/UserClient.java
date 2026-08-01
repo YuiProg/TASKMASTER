7469f5df78047766538f87435034b0f223e1b38a
YuiProg <javierpatrick79@gmail.com>
2026-08-01 12:47:53 +0800

    report feign fix

--- taskmaster-backend/task-master-reports/src/main/java/com/example/reports_service/reports/client/UserClient.java
+++ taskmaster-backend/task-master-reports/src/main/java/com/example/reports_service/reports/client/UserClient.java
@@ -6,8 +6,8 @@ import com.example.reports_service.reports.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//@FeignClient(name = "backend-core-client", url = "${services.backend.url}")
-@FeignClient(name = "backend-user-client", url = "http://localhost:8080/api/v1")
+@FeignClient(name = "backend-core-client", url = "${services.backend.url}")
+//@FeignClient(name = "backend-user-client", url = "http://localhost:8080/api/v1")
 public interface UserClient {
     @GetMapping("/getUserById/{id}")
     ApiResponseModel<UserDTO> getUserById (@PathVariable String id);
