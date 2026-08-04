a3f0415586b286d408c23f54118a202cbffaa1bf
YuiProg <javierpatrick79@gmail.com>
2026-08-04 21:27:40 +0800

    email enhancements

--- taskmaster-backend/task-master-core/src/main/java/com/example/backend/repository/TaskRepository.java
+++ taskmaster-backend/task-master-core/src/main/java/com/example/backend/repository/TaskRepository.java
@@ -17,8 +17,8 @@ public interface TaskRepository extends JpaRepository<Task, String> {
     @Query("SELECT t FROM Task t JOIN Project p ON t.project.id = p.id WHERE p.id = :id AND t.del = 0 AND t.archived = 0 ORDER BY t.createdAt DESC")
     List<Task> getTaskOnProject (@Param("id") String id);
 
-    @Query("SELECT t FROM Task t WHERE (t.status = 'OPEN' OR t.assignee IS NULL) AND t.del = 0 AND t.archived = 0 ORDER BY t.createdAt DESC")
-    List<Task> getAllOpenTask();
+    @Query("SELECT t FROM Task t WHERE t.assignee.id = :id AND t.del = 0 AND t.status = 'OPEN' AND t.archived = 0 ORDER BY t.createdAt DESC")
+    List<Task> getAllOpenTask(@Param("id") String id);
 
     @Modifying
     @Transactional
