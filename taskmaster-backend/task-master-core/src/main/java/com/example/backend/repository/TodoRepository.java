0846fa36822c3ec969861376bfa3b937c3892c48
YuiProg <javierpatrick79@gmail.com>
2026-08-13 22:47:42 +0800

    bug fix in todo

--- taskmaster-backend/task-master-core/src/main/java/com/example/backend/repository/TodoRepository.java
+++ taskmaster-backend/task-master-core/src/main/java/com/example/backend/repository/TodoRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, String> {
 
-    @Query("SELECT t FROM Todo t WHERE t.createdBy.id = :userId AND t.del = 0 AND t.finished = 0")
+    @Query("SELECT t FROM Todo t WHERE t.createdBy.id = :userId AND t.del = 0")
     List<Todo> getUserTodos(String userId);
 
-    @Query("SELECT t FROM Todo t WHERE t.id = :id AND t.del = 0 AND t.finished = 0")
+    @Query("SELECT t FROM Todo t WHERE t.id = :id AND t.del = 0")
     Todo getTodoById(String id);
 }
